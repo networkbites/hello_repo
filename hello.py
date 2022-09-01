d6abde153b37c49cfa1954e4a02b09e4ee2374cb
Will Wetherman <will@packetoverflow.net>
2022-09-01 22:31:49 +0100

    Testing git merge

--- hello.py
+++ hello.py
@@ -2,7 +2,6 @@ import sys
 
 print("Hello Git world!")
 print(f"Using Python version {sys.version}")
-
 print("We hope you like using Git.")
-
 print("Testing git branch")
+print("Testing git merge")
